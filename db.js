90e071941a8b5ffdd2410ce77fa5d2442ec57673
LOKESH MELLAKANTI <143256558+Lokeshd200@users.noreply.github.com>
2024-12-06 19:05:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const sqlite3 = require('sqlite3')
 const dotEnv= require ('dotenv');
 const path = require ('path')
+dotEnv.config();
 
 const dbPath = path.join(__dirname,"database.db")
 
-const db = new sqlite3.Database('./database.db', (err) => {
+const db = new sqlite3.Database(process.env.DATABASE_URL, (err) => {
     if (err) {
       console.error('Error connecting to SQLite database:', err.message);
     } else {
